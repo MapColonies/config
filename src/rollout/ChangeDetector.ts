f2363e806c842709f613a9baa8e9172ccac05cb0
netanelC <netanel153@gmail.com>
2026-06-01 09:58:27 +0300

    feat: add jitter

--- src/rollout/ChangeDetector.ts
+++ src/rollout/ChangeDetector.ts
@@ -1,3 +1,4 @@
+import { JITTER_PERCENTAGE } from '../constants';
 import { getRemoteConfig } from '../httpClient';
 import { BaseOptions } from '../types';
 import { createDebug } from '../utils/debug';
@@ -22,21 +23,39 @@ export class ChangeDetector {
   }
 
   public start(): void {
-    const interval = this.options.pollIntervalMs;
-    debug('Starting change detector with interval %d ms', interval);
-
-    this.timer = setInterval(() => {
-      this.poll().catch((err) => {
-        debug('Error during polling: %s', (err as Error).message);
-      });
-    }, interval);
+    debug('Starting change detector');
+    this.scheduleNextPoll();
   }
 
   public stop(): void {
     if (this.timer) {
-      clearInterval(this.timer);
+      clearTimeout(this.timer);
       this.timer = undefined;
     }
+  }
+
+  private scheduleNextPoll(): void {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    const baseInterval = this.options.pollIntervalMs!;
+    const jitter = baseInterval * JITTER_PERCENTAGE;
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    const randomJitter = (Math.random() * 2 - 1) * jitter;
+    const nextInterval = baseInterval + randomJitter;
+
+    debug('Scheduling next poll in %d ms', nextInterval);
+    this.timer = setTimeout(() => {
+      this.poll()
+        .catch((err) => {
+          debug('Error during polling: %s', (err as Error).message);
+        })
+        .finally(() => {
+          if (this.timer !== undefined) {
+            this.scheduleNextPoll();
+          }
+        });
+    }, nextInterval);
   }
 
   private async poll(): Promise<void> {
